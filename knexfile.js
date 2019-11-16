5c60577dc34a3825b8ab4d2216a7b6fd683f573d
Aaron Clark <aaronpclark92@gmail.com>
2019-11-16 13:52:41 -0600

    - added postgres dependency - added start script in package.json - added production script in knexfile

--- knexfile.js
+++ knexfile.js
@@ -31,4 +31,14 @@ module.exports = {
       directory: './data/seeds',
     },
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
 };
