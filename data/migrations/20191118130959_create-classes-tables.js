f24650882bc3e197bba1e22e8ee713559c110826
lowell1 <lowelljacobs@mail.com>
2019-11-18 13:49:33 -0800

    user-classes table migration

--- data/migrations/20191118130959_create-classes-tables.js
+++ data/migrations/20191118130959_create-classes-tables.js
@@ -2,10 +2,10 @@
 exports.up = function(knex) {
   return knex.schema.createTable("classes", table => {
       table.increments();
-      table.string("class_name", 128).unique();
+      table.string("class_name", 128).unique().notNullable();
   })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("classes");
 };
