1d1ea76c5f5cb548cf9aca2687e60f73f19be6c6
Aaron Clark <aaronpclark92@gmail.com>
2019-11-18 17:13:36 -0600

    fixed hyphen in user_classes migration

--- data/migrations/20191118132844_user-classes-table.js
+++ data/migrations/20191118132844_user-classes-table.js
@@ -1,6 +1,6 @@
 
 exports.up = function (knex) {
-  return knex.schema.createTable("user-classes", table => {
+  return knex.schema.createTable("user_classes", table => {
     table.increments();
     table.integer("user_id").unsigned().notNullable().references("id").inTable("users").onDelete('cascade');
     table.integer("class_id").unsigned().notNullable().references("id").inTable("classes").onDelete('cascade');
