b0c8d2a1e42a4c9777e22bfad01a123f8e6d1c6d
Aaron Clark <aaronpclark92@gmail.com>
2019-11-18 17:05:19 -0600

    migration for test_question_answer created

---
+++ data/migrations/20191118170021_test_question_answers.js
@@ -0,0 +1,19 @@
+
+exports.up = function (knex) {
+  return knex.schema.createTable('test_question_answer', tbl => {
+    tbl.icrements();
+    tbl
+      .varchar('correct_answer', 255)
+      .notNullable();
+    tbl
+      .integer('question_id', 255)
+      .unsigned()
+      .references('id')
+      .inTable('test_questions')
+      .onDelete('cascade')
+  })
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('test_question_answer')
+};
