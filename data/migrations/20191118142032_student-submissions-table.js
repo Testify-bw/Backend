ea80a64da230bd07151d044ec25fba35dd97656e
lowell1 <lowelljacobs@mail.com>
2019-11-18 14:52:10 -0800

    fixed foreign keys

--- data/migrations/20191118142032_student-submissions-table.js
+++ data/migrations/20191118142032_student-submissions-table.js
@@ -2,8 +2,8 @@
 exports.up = function(knex) {
   return knex.schema.createTable("student_submissions", table => {
     table.increments();
-    table.integer("student_id").notNullable().references("id").inTable("users");
-    table.integer("test_id").notNullable().references("id").inTable("tests");
+    table.integer("student_id").unsigned().notNullable().references("id").inTable("users").onDelete('cascade');
+    table.integer("test_id").unsigned().notNullable().references("id").inTable("tests").onDelete('cascade');
     table.integer("submission_number");
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.float("percent_correct", {precision: 2}).notNullable();
