e7d99b66b4225bf3a207e3e3a54231642745a4b1
lowell1 <lowelljacobs@mail.com>
2019-11-19 15:10:28 -0800

    made endpoint for retrieving tests, fixed some question_ids for choices in seed file

--- models/test-model.js
+++ models/test-model.js
@@ -3,13 +3,26 @@ const db = require("../data/db-config");
 module.exports = {
     findTestById: id => {
         return db("tests")
-        .select("test_name")
-        .where({id: id})
-        .first();
-    },
+        .select("test_name", "question_text", "short_answer", "choice", "question_id")
+        // .select("test_name", "question_text", "short_answer")
+        .join("test_questions", "tests.id", "=", "test_id")
+        .leftJoin("question_choices", "test_questions.id", "question_id")
+        .where({"tests.id": id});
+        // .first();
+    }
+    ,
     findQuestionsByTestId: testId => {
-        return db("test_questions")
-        .select("question_text", "short_answer")
-        .where({test_id: testId})
+        db("tests")
+        .select("test_name", "question_text", "short_answer")
+        // .select("test_name", "question_text", "short_answer")
+        .join("test_questions", "tests.id", "=", "test_id")
+        .join("question_choices", "question_id", "=", "test_questions.id")
+        .where({"tests.id": testId})
+        .then(q => console.log(q))
+    //     return db("test_questions")
+    //     .select("question_text", "short_answer", "choice")
+    //     .join("question_choices", "test_questions.id", "question_id")
+    //     .groupBy("test_questions.id")
+    //     .where({test_id: testId});
     }
 }
