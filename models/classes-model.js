f257b611f8868e783a4f9e9d761455a4ac91b6fb
Aaron Clark <aaronpclark92@gmail.com>
2019-11-20 11:16:52 -0600

    added function to add user to class in classes-model

--- models/classes-model.js
+++ models/classes-model.js
@@ -3,7 +3,8 @@ const db = require('../data/db-config');
 module.exports = {
   get,
   getById,
-  add
+  add,
+  addToClass
 }
 
 function get() {
@@ -22,5 +23,21 @@ function add(newClass) {
     .then(ids => {
       const [id] = ids;
       return getById(id)
+    })
+}
+
+function addToClass(user_id, class_id) {
+  const relation = {
+    user_id,
+    class_id
+  }
+  return db('user_classes')
+    .insert(relation, 'id')
+    .then(ids => {
+      return db('user_classes as uc')
+        .join('classes as c', 'uc.class_id', 'c.id')
+        .join('users as u', 'uc.user_id', 'u.id')
+        .select('u.username', 'c.class_name')
+        .where('uc.id', id)
     })
 }
