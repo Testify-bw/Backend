f994e47708619df56c5c5673fb4ee5c4e9bc4456
Aaron Clark <aaronpclark92@gmail.com>
2019-11-20 11:44:07 -0600

    added endpoint to get student submission with answers

---
+++ routes/submission-router.js
@@ -0,0 +1,39 @@
+const requireValidToken = require("../middleware/requireValidToken");
+const router = require("express").Router();
+const Submissions = require("../models/submissions-model");
+const ensureUserHasTest = require("../middleware/ensureUserHasTest");
+
+// get all submissions
+router.get('/', requireValidToken, (req, res) => {
+  Submissions.get()
+    .then(submissions => {
+      res.status(200).json(submissions)
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: `Error retrieving submissions from the database.`,
+        error: err.toString()
+      });
+    })
+});
+
+// get submission and response by test_id
+router.get('/:test_id', requireValidToken, (req, res) => {
+
+  try {
+    const { test_id } = req.params;
+    const submission = await Submissions.getByTest(test_id);
+    submission.responses = await Submissions.getResponsesBySubmission(submission.id)
+      .then(sub => {
+        res.status(200).json(sub)
+      })
+  } catch (err) {
+    res.status(500).json({
+      message: `Error retrieving submission from the database.`,
+      error: err.toString()
+    });
+  }
+
+})
+
+module.exports = router;
